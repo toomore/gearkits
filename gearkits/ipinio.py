b074dfbc038ff2fd658c34164df6b1d44378767d
Toomore Chiang <toomore0929@gmail.com>
2022-05-15 17:01:08 +0800

    init

---
+++ gearkits/ipinio.py
@@ -0,0 +1,23 @@
+''' IPInfo '''
+# pylint: disable=arguments-renamed,arguments-differ
+from requests import Session
+
+
+class IPInfo(Session):
+    ''' IPInfo '''
+
+    def __init__(self, token: str):
+        super().__init__()
+        self.token = token
+
+    def get(self, ip: str):
+        ''' Get info
+
+        :param str ip: ip
+
+        '''
+        headers = {
+            'Authorization': f'Bearer {self.token}',
+        }
+        return super().get(f'https://ipinfo.io/{ip}', headers=headers)
+
